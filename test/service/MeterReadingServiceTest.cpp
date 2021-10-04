35784340514ef3411f1595d9c35c4102b53be24d
superxcgm <superxcgm@gmail.com>
2021-10-04 19:37:45 +0800

    Add MeterReadingServiceTest

---
+++ test/service/MeterReadingServiceTest.cpp
@@ -0,0 +1,21 @@
+#include <gmock/gmock.h>
+#include <rest/service/MeterReadingService.h>
+
+TEST(MeterReadingServiceTest, GetReadingsShouldReturnNullGivenMeterIdThatDoesNotExist) {
+  std::unordered_map<std::string, std::vector<ElectricityReading>> meterAssociatedReadings;
+  MeterReadingService meterReadingService{meterAssociatedReadings};
+
+  EXPECT_FALSE(meterReadingService.getReadings("unknown-id").has_value());
+}
+
+TEST(MeterReadingServiceTest, GetReadingsShouldReturnMeterReadingsGivenMeterReadingThatExists) {
+  std::unordered_map<std::string, std::vector<ElectricityReading>> meterAssociatedReadings;
+  MeterReadingService meterReadingService{meterAssociatedReadings};
+  std::vector<ElectricityReading> emptyReadings;
+  meterReadingService.storeReadings("some-id", emptyReadings);
+
+  auto readings = meterReadingService.getReadings("some-id");
+
+  EXPECT_TRUE(readings.has_value());
+  EXPECT_TRUE(readings.value().empty());
+}
